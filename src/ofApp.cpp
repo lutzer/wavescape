dfd89e6bca91e753c621f9025b9681e1ff021f74
lutzer <in_the_fishtank@gmx.de>
2017-06-25 17:00:20 +0200

    cleaned up comments

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -61,7 +61,7 @@ void ofApp::setup(){
 //--------------------------------------------------------------
 void ofApp::update(){
     
-    float dt = ofGetElapsedTimef();
+    //float dt = ofGetElapsedTimef();
     
     
     
@@ -164,35 +164,5 @@ void ofApp::audioOut(float * output, int bufferSize, int nChannels){
 
         phase = fmod(phase + phaseStep, 1.0);
     }
-
-    //ofLog() << phase;
-
-
-
-//    //pan = 0.5f;
-//    float leftScale = 1 - pan;
-//    float rightScale = pan;
-//
-//    // sin (n) seems to have trouble when n is very large, so we
-//    // keep phase in the range of 0-TWO_PI like this:
-//    while (phase > TWO_PI){
-//        phase -= TWO_PI;
-//    }
-//
-//    if ( bNoise == true){
-//        // ---------------------- noise --------------
-//        for (int i = 0; i < bufferSize; i++){
-//            lAudio[i] = output[i*nChannels    ] = ofRandom(0, 1) * volume * leftScale;
-//            rAudio[i] = output[i*nChannels + 1] = ofRandom(0, 1) * volume * rightScale;
-//        }
-//    } else {
-//        phaseAdder = 0.95f * phaseAdder + 0.05f * phaseAdderTarget;
-//        for (int i = 0; i < bufferSize; i++){
-//            phase += phaseAdder;
-//            float sample = sin(phase);
-//            lAudio[i] = output[i*nChannels    ] = sample * volume * leftScale;
-//            rAudio[i] = output[i*nChannels + 1] = sample * volume * rightScale;
-//        }
-//    }
     
 }
